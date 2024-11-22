c590fad6994f6e0509150222357971263208fe1b
ITSC <itsc@example.com>
2024-11-22 19:54:01 +0900

    깃허브 테스트

---
+++ section03/ex03-1-escape.py
@@ -0,0 +1,18 @@
+'''
+이스케이프 문자 (escape character)
+    -특수 기능을 가진 제어 문자
+    -백 슬래시(\)로 시작
+
+    \t: tab
+    \b: backspace
+    \': 작은 따옴표
+    \": 큰 따옴표
+    \n: 개행 (줄바꿈)
+'''
+
+#1. 이스케이프 문자 활용
+pokemon_info = 'ID: \'피카츄\'\n타입: \'전기\'\t Level: 25'
+print(pokemon_info)
+
+
+
