7010efefb773e2d9e52123cfbb522cf7a9fcfdaa
ITSC <itsc@example.com>
2024-12-06 19:36:18 +0900

    11/22

---
+++ section14/Ex14-5-jsonWriter.py
@@ -0,0 +1,36 @@
+'''
+파일명: Ex14-5-jsonWriter.py
+
+JSON(Javascript Object Notation)
+    데이터를 키와 값의 쌍을 중괄호({})로 묶어 표현하는 형식
+    - 딕셔너리와 비슷하다
+    - 구조 {K: V}
+'''
+
+import json
+
+dict_list = [
+    {
+        'name':'홍길동',
+        'age':20,
+        'spec':[
+            175.5,
+            70.5
+        ]
+    },
+    {
+        'name':'alice',
+        'age':21,
+        'spec':[
+            168.5,
+            60.5
+        ]
+    }
+]
+
+json_string = json.dumps(dict_list, indent=4, ensure_ascii=False)
+
+with open('dictList.json', 'w', encoding='UTF-8') as file:
+    file.write(json_string)
+
+print('dictList.json 파일이 생성되었습니다.')
